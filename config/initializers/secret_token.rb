411b7fa9cabcfa5ee654e6a220413039bb19c87f
nuxa <holy.edge.nuxaxtuxtu@gmail.com>
2015-04-02 03:15:04 +0000

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = 'b4b37db9f0351d11893697dd3f62fb2d6100a5f343f94e8c7b2db109635a2fd83689788c264960335a156d51f711970a7699830f0ced67ad75edf42701c8d6af'
